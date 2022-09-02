2951f9f301f470c7ab7ff0cae42d176447ac652f
Adson Gomes Oliveira <agdev@DESKTOP-0N655A3.localdomain>
2022-09-02 11:33:24 -0300

    <feat>: Created first reducer
    Sign Up Reducer

---
+++ src/redux/reducers/signUpReducer.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+  username: {},
+};
+
+const signUpReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default signUpReducer;
