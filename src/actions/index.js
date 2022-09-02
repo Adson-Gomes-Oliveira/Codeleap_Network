3c70f32ea67442b0ea3b15cb729c5cfdd6956f68
Adson Gomes Oliveira <agdev@DESKTOP-0N655A3.localdomain>
2022-09-02 11:44:44 -0300

    <feat>: Created first action for user

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+const CREATE_USER_TYPE = 'CREATE_USER';
+
+export const createUser = (username) => {
+  return {
+    type: CREATE_USER_TYPE,
+    user: username,
+  }
+};
--- src/redux/reducers/signUpReducer.js
+++ src/redux/reducers/signUpReducer.js
@@ -1,9 +1,17 @@
+import CREATE_USER_TYPE from '../../actions/index';
+
 const INITIAL_STATE = {
   username: {},
 };
 
 const signUpReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case CREATE_USER_TYPE: {
+      return {
+        ...state,
+        user: action.user,
+      }
+    }
     default:
       return state;
   }
